54e79f7814288cdcf9151c73d0e741e1948de7ad
Raito <145450869+Q7nurs3vim@users.noreply.github.com>
2024-02-16 12:06:26 +0300

    Add files via upload

---
+++ neat_esboo_jarv1.ino
@@ -0,0 +1,21 @@
+void setup()
+{
+  pinMode(A0, INPUT);
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int isik = analogRead(A0);;  
+  Serial.println(isik);
+ if(isik < 200)
+ {
+   digitalWrite(2,1);
+ }
+  else 
+  {
+    digitalWrite(2,0);
+  }
+  delay(10);
+}
